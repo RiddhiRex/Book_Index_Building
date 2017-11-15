e4793b24695593fe44b28f680d752b7c76f29b79
gaganso <gagan.somashekar@gmail.com>
2017-11-14 23:17:40 -0500

    moved dataframe generation to a .py file

---
+++ dataframe_generation.py
@@ -0,0 +1,44 @@
+import nltk, collections
+import pandas as pd
+import numpy as np
+
+
+def generate_dataframe(filename,feat_dict,feature_list_from_tex):
+    df = pd.DataFrame()
+    candidate_list = feat_dict["candidate_list"]
+    frequency = collections.Counter()
+    for w in candidate_list:
+        frequency[w] += 1
+        
+    tagged = nltk.pos_tag(frequency.keys())
+    
+    notpos = ['VB','VBP','PRP','IN','RB','DT','WDT','WP','WRB','UH','TO','RBR','RBS','POS','MD','EX','WP$','PRP$','$']
+    w1 = list(filter(lambda word_tag: word_tag[1]  not in notpos, tagged))
+    
+    word = []
+    pos = []
+    count = []
+
+    
+    for each in w1:
+        word.append(each[0])
+        pos.append(each[1])
+    df['word']=word
+    df['pos']=pos
+    for each in word:
+        count.append(frequency[each])
+    df['wordcount'] = count
+    
+    cols = ['NN','NNP','NNS','VBG','VBD','VBN','VBZ','VBP','VB','CD','CC','LS','JJ','JJS','JJR','PDT','RP','FW','NNPS']
+    df1 = pd.DataFrame(0, index=np.arange(len(df['word'])), columns=cols)
+    df2 = pd.concat([df, df1], axis=1)
+    
+    for idx, row in df2.iterrows():
+        pos = row['pos']
+        df2.set_value(idx, pos, 1)
+    
+    for feat in feature_list_from_tex:
+        df2[feat] = 0
+        df2.loc[df2['word'].isin(feat_dict[feat]),feat]=1
+    df2["filename"] = filename
+    return df2
